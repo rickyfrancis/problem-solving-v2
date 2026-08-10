9049265894f609062c46f1c98b5460a892d7af36
Ricky Francis Rozario <rickyfrancisrozario@gmail.com>
2026-08-10 10:49:44 +0200

    feat: implement solution for Leetcode 217 - Contains Duplicate

---
+++ problems/0002-contains-duplicate/solution.py
@@ -0,0 +1,15 @@
+# Leetcode 217. Contains Duplicate
+
+def containsDuplicate(self, nums: List[int]) -> bool:
+    hashset = set()
+
+    for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+    return False
+
+
+nums = [1,1,1,3,3,4,3,2,4,2]
+
+print(containsDuplicate(nums))
